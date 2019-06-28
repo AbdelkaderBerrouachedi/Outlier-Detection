f0fc89c4be426ca72adc41afe1b22b9517df7d91
Antonio Ritacco <ritacco.ant@gmail.com>
2019-06-28 12:53:22 +0200

    Using as training data some skleanr outliers datasets

--- Classify_Outliers.py
+++ Classify_Outliers.py
@@ -30,33 +30,47 @@ test_data_1 = pd.read_csv('data/Synth/circles_moons_IGNG_outliers.csv')#pd.read_
 test_data_2 = pd.read_csv('data/Synth/moons_circles_IGNG_outliers.csv')
 
 test_data_3 = pd.read_csv('data/Synth/blobs_circles_IGNG_outliers.csv')
-# train_data_4 = pd.read_csv('data/Synth/circles_blobs_IGNG_outliers.csv')
 test_data_4 = pd.read_csv('data/Synth_ES/moons_circles_IGNG_outliers.csv')
 
 test_data_5 = pd.read_csv('data/Synth/moons_blobs_IGNG_outliers.csv')
 test_data_6 = pd.read_csv('data/Synth/blobs_moons_IGNG_outliers.csv')
 test_data_7 = pd.read_csv('data/Synth/circles_moons_IGNG_outliers.csv')
+test_data_8 = pd.read_csv('data/Synth/circles_blobs_IGNG_outliers.csv')
+
+
 
 train_data_1 = pd.read_csv('data/Synth/Train/0.csv_IGNG_outliers.csv')#pd.read_csv('data/Cardio/IGNG_outliers.csv')
+train_data_1 = scaler.fit_transform(train_data_1)
+
 train_data_2 = pd.read_csv('data/Synth/Train/1.csv_IGNG_outliers.csv')
+train_data_2 = scaler.fit_transform(train_data_2)
 
 train_data_3 = pd.read_csv('data/Synth/Train/2.csv_IGNG_outliers.csv')
+train_data_3 = scaler.fit_transform(train_data_3)
+
 # train_data_4 = pd.read_csv('data/Synth/circles_blobs_IGNG_outliers.csv')
 train_data_4 = pd.read_csv('data/Synth/Train/3.csv_IGNG_outliers.csv')
-train_data_5 = pd.read_csv('data/Synth/Train/4.csv_IGNG_outliers.csv')
+train_data_4 = scaler.fit_transform(train_data_4)
 
-train_data = pd.concat((train_data_1, train_data_2, train_data_4), axis=0)
+train_data_5 = pd.read_csv('data/Synth/Train/4.csv_IGNG_outliers.csv')
+train_data_5 = scaler.fit_transform(train_data_5)
+
+
+train_data = np.concatenate((train_data_1, train_data_2, train_data_3, train_data_4, train_data_5), axis=0)
 # train_data2 = pd.read_csv('data/Synth/Moons/blobs_IGNG_outliers.csv', usecols=['min_distances','avg_k_distances','max_k_distances',
 #                                                                 'outlier_K_factor','lof_clusters','cluster_sparsity','label'])#pd.read_csv('data/ALOI/IGNG_outliers.csv')
 # train_data3 = pd.read_csv('data/Cardio/IGNG_outliers.csv', usecols=['min_distances','avg_k_distances','max_k_distances',
 #                                                                 'outlier_K_factor','lof_clusters','cluster_sparsity','label'])#pd.r
-test_data_input = pd.read_csv('data/Synth/Train/2.csv', header=None).iloc[:, :2]
+# test_data_input = pd.read_csv('data/Synth/circles_blobs_input_data.csv', header=None)
 # labels_data = np.concatenate([train_data.iloc[:, -1].values, test_data.iloc[:, -1].values])
 # print(train_data.shape, test_data.shape, test_data_input.shape)
 # train_data = train_data_6
-test_data = train_data_3
+test_data_input = pd.read_csv('data/Cardio/CardioData.csv', header=None)
 
-columns = list(train_data.columns)
+test_data = pd.read_csv('data/Cardio/IGNG_outliers.csv', usecols=['min_distances','avg_k_distances','max_k_distances',
+                                                                'outlier_K_factor','lof_clusters','cluster_sparsity','label'])
+
+# columns = list(test_data.columns)
 
 # train_data = pd.concat((train_data1, train_data2, train_data3),axis=0)
 # test_data = pd.read_csv('data/ALOI/IGNG_outliers.csv', usecols=['min_distances','avg_k_distances','max_k_distances',
@@ -97,16 +111,17 @@ compute_metrics(clf, X_test_data, y_test_data)
 # compute_metrics(clf, test_data_ae, test_data[:, -1])
 
 # print('******** SVM **********')
-# clf = SVC(random_state=0, C=1).fit(train_data[:, :-1], train_data[:, -1])
+# clf = SVC(random_state=0, C=1, gamma='scale').fit(X_train_data, y_train_data)
 # print('*** TRAIN ***')
-# compute_metrics(clf, train_data[:, :-1], train_data[:, -1])
+# compute_metrics(clf, X_train_data, y_train_data)
 # print('*** TEST ***')
-# compute_metrics(clf, test_data[:, :-1], test_data[:, -1])
+# compute_metrics(clf, X_test_data, y_test_data)
 
 # train_score = clf.score(train_data[:, :-1], train_data[:, -1])
 # test_score = clf.score(test_data[:, :-1], test_data[:, -1])
 # print("Mean train accuracy: {:.4f} , Mean test accuracy: {:.4f}".format(train_score, test_score))
 y_pred_test = clf.predict(X_test_data)
+df_result = pd.DataFrame(data=np.concatenate((y_pred_test.reshape(-1, 1), y_test_data.reshape(-1, 1)),axis=1), columns=['pred', 'actual'])
 recon_outliers = test_data_input[y_pred_test == 1]
 # n_outliers = 250
 fig, ax = plt.subplots()
